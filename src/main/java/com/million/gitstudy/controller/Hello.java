075188dcb53dd2384a0a9ff2e925b03f66cd214e
shilu <1658634188@qq,com>
2021-10-10 12:42:39 +0800

    这是我的第一次git提交

---
+++ src/main/java/com/million/gitstudy/controller/Hello.java
@@ -0,0 +1,11 @@
+package com.million.gitstudy.controller;
+
+/**
+ * @Author: studyboy
+ * @Date: 2021/10/10  12:40
+ */
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("hello，git");
+    }
+}
