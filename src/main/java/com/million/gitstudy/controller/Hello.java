7e0d478c57529e7c13238e833a792ce816927944
shilu <1658634188@qq,com>
2021-10-10 13:25:22 +0800

    新建了一个分支

--- src/main/java/com/million/gitstudy/controller/Hello.java
+++ src/main/java/com/million/gitstudy/controller/Hello.java
@@ -7,5 +7,6 @@ package com.million.gitstudy.controller;
 public class Hello {
     public static void main (String[] args) {
         System.out.println("hello，git");
+        System.out.println("我新建了一个分支");
     }
 }
